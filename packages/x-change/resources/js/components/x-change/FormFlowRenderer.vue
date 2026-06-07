deaadbefdeb70a3430f572a3d8447b6f514e2c7e
Lester Hurtado <lester@lyflyn.net>
2026-06-07 10:11:42 +0800

    refactor: remove form flow renderer metadata diagnostics

--- packages/x-change/resources/js/components/x-change/FormFlowRenderer.vue
+++ packages/x-change/resources/js/components/x-change/FormFlowRenderer.vue
@@ -60,26 +60,6 @@ watch(
 
 <template>
     <div data-testid="form-flow-renderer">
-        <div data-testid="form-flow-key">
-            {{ formFlow.key }}
-        </div>
-
-        <div data-testid="form-flow-owner">
-            {{ formFlow.owner ?? '' }}
-        </div>
-
-        <div data-testid="form-flow-source">
-            {{ formFlow.source ?? '' }}
-        </div>
-
-        <div data-testid="form-flow-field-count">
-            {{ formFlow.fields.length }}
-        </div>
-
-        <div data-testid="form-flow-stage-count">
-            {{ formFlow.stages.length }}
-        </div>
-
         <div data-testid="form-flow-fields">
             <div
                 v-for="field in formFlow.fields"
