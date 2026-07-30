151e92dd84ea505c65945af018c4667c6f74496e
Lester Hurtado <lester@lyflyn.net>
2026-07-30 17:03:35 +0800

    campaign: keep worksheet wrapper transparent

--- packages/x-change/resources/js/pages/x-change/cockpit/CampaignWorksheet.vue
+++ packages/x-change/resources/js/pages/x-change/cockpit/CampaignWorksheet.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import CampaignWorksheet from '../../../cockpit/pages/CampaignWorksheet.vue';
 
-defineProps<InstanceType<typeof CampaignWorksheet>['$props']>();
+defineOptions({ inheritAttrs: false });
 </script>
 
-<template><CampaignWorksheet v-bind="$props" /></template>
+<template><CampaignWorksheet v-bind="$attrs" /></template>
